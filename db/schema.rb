8283e6046c0ffabf5d5f2e7dda2d77599d09a78d
Paul Schoen <pschoen325@gmail.com>
2016-11-29 18:24:30 -0500

    made models suggestions and restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129230207) do
+ActiveRecord::Schema.define(version: 20161129232326) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "suggestions", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
