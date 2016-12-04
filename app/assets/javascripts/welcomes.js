de2c41422e46285cd3a2599eccd221ffa3ebe009
Paul Schoen <pschoen325@gmail.com>
2016-12-04 17:32:54 -0500

    spike removed jquery plugin causing web page not to load, added hambuger menu

--- app/assets/javascripts/welcomes.js
+++ app/assets/javascripts/welcomes.js
@@ -1,7 +1,5 @@
 $(document).on('turbolinks:load', function() {
-  $('#video').tubular({videoId: 'l-PxQsuNql0', mute: true});
-
 });
 $(window).bind("load", function() {
-   $('div.hidden').fadeIn(5000).removeClass('hidden');
+   $('div.hidden').fadeIn(2000).removeClass('hidden');
 });
