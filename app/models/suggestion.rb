f63e77f286e7bad30093523109f175a5593a37a7
Paul Schoen <pschoen325@gmail.com>
2016-12-07 18:36:08 -0500

    spike added menu items to suggestions and added a counter to restaurant partial

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,6 +1,8 @@
 class Suggestion < ApplicationRecord
     include Yelp::V2::Business::Request
 
+    scope :yelp_id_match, -> (business_id) {where('yelp_id = ?', business_id)}
+
     def call_yelp
         client = Yelp::Client.new
         request = Id.new(
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206174841) do
+ActiveRecord::Schema.define(version: 20161207195630) do
 
   create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
